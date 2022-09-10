48e69d94ba79c1ebf1aec36a3449db0b2684bfa6
Dev Lion Of Kurdistan <76536335+lionofkurdistan@users.noreply.github.com>
2022-09-10 22:01:02 +0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('.img');
+let continer = document.querySelector('.continer');
+
+
+function phonesm(phone){
+  img.src = phone;
+}
+
+function colors(color){
+  continer.style.background = color;
+}
